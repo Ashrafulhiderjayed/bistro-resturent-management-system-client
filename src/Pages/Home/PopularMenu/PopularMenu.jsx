a8273656165f3c206e8ce4448ada712e352c414f
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-08-10 15:35:13 +0600

    filtered and show popular data

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() =>{
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popularItems = data.filter(item => item.category === 'popular');
+            setMenu(popularItems);
+            console.log(popularItems);
+        })
+    },[])
     return (
-        <div>
-            
-        </div>
+        <section>
+            <SectionTitle
+            heading="From Our Menu"
+            subHeading="Popular Items"
+            ></SectionTitle>
+        </section>
     );
 };
 
