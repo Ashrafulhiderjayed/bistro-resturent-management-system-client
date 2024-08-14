d464d7376466099df88cf3b6292910cffae2ae15
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-08-14 14:54:28 +0600

    maked menu workable

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/menu-bg.png";
 
 const Menu = () => {
     return (
@@ -7,7 +8,8 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
-            <Cover />
+            <Cover img={menuImg} title="our menu" />
+
         </div>
     );
 };
