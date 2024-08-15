0e33083da44757491cda4cbc6b31362e077f1bb2
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-08-15 15:58:39 +0600

    useMenu data fetch

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 
 
 const useMenu = () =>{
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+       .then(res => res.json())
+       .then(data => {
+        const popularItems = data.filter(item => item.category === 'category');
+        setMenu(popularItems);
+       });
+    }, []);
+    return [menu]
 
 }
 export default useMenu;
