a11064a24616e2a75c8ff107a845250f170b3fcb
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-08-25 09:25:45 +0600

    firebase authentication implemented using context api

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
@@ -9,9 +10,43 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // observe auth state change
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+            setLoading(false);
+        });
+
+
+        return () =>{
+            return unsubscribe();
+        }
+
+
+    }, [])
+
+
     const authInfo = {
         user,
         loading,
+        createUser,
+        logOut,
     }
     return (
         <AuthContext.Provider value={authInfo}>
