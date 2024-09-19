b7f47dcc016485daf575dd66dda161eae38cfe45
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-19 14:20:14 +0600

    user delete api implement

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,16 +1,49 @@
 import { FaTrashAlt, FaUsers } from 'react-icons/fa';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+
+    const handleMakeAdmin = (user) => {
+
+    }
+
+    const handleDeleteUser = (user) => {
+        console.log(user);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <section>
             <div className="flex justify-evenly my-4">
@@ -38,31 +71,31 @@ const AllUsers = () => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            users.map((user, index) =><tr key={index}>
-                            <th>
-                                {index + 1} 
-                            </th>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle h-12 w-12">
-                                            <img
-                                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                alt="Avatar Tailwind CSS Component" />
+                            users.map((user, index) => <tr key={index}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                                    alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{user?.name}</div>
+                                            <div className="text-sm opacity-50">{user?.email}</div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">{user?.name}</div>
-                                        <div className="text-sm opacity-50">{user?.email}</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                {user.role === 'admin' ? 'admin' : <button></button>}
-                            </td>
-                            <td>Purple</td>
-                            <td>
-                                    { user.role === 'admin' ? 'Admin' : <button
+                                </td>
+                                <td>
+                                    {user.role === 'admin' ? 'admin' : <button></button>}
+                                </td>
+                                <td>Purple</td>
+                                <td>
+                                    {user.role === 'admin' ? 'Admin' : <button
                                         onClick={() => handleMakeAdmin(user)}
                                         className="btn btn-lg bg-orange-500">
                                         <FaUsers className="text-white 
@@ -76,9 +109,9 @@ const AllUsers = () => {
                                         <FaTrashAlt className="text-red-600"></FaTrashAlt>
                                     </button>
                                 </td>
-                        </tr>)
+                            </tr>)
                         }
-                        
+
                     </tbody>
                 </table>
             </div>
