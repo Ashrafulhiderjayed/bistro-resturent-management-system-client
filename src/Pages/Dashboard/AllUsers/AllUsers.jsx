8f7423fae87a06fb71bce49ef3dc4f4cc7eca3f4
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-19 16:29:00 +0600

    user make admin api implement

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -13,9 +13,23 @@ const AllUsers = () => {
         }
     })
 
-    const handleMakeAdmin = (user) => {
-
+    const handleMakeAdmin = user =>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res =>{
+            console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
     }
+
 
     const handleDeleteUser = (user) => {
         console.log(user);
@@ -90,12 +104,9 @@ const AllUsers = () => {
                                         </div>
                                     </div>
                                 </td>
+                                <td>{user?.email}</td>
                                 <td>
-                                    {user.role === 'admin' ? 'admin' : <button></button>}
-                                </td>
-                                <td>Purple</td>
-                                <td>
-                                    {user.role === 'admin' ? 'Admin' : <button
+                                { user.role === 'admin' ? 'Admin' : <button
                                         onClick={() => handleMakeAdmin(user)}
                                         className="btn btn-lg bg-orange-500">
                                         <FaUsers className="text-white 
