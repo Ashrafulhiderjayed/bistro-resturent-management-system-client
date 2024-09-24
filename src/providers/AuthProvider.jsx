5debb1de344a115dcb42dc6b59de8fa8ce2026d1
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-24 17:19:01 +0600

    set authorization token as we used local storage for jwt

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
@@ -41,6 +42,20 @@ const AuthProvider = ({children}) => {
     useEffect( () =>{
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            if(currentUser){
+                // get token and store client
+                const userInfo = {email: currentUser.email};
+                useAxiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+            }
+            else{
+                // remove token (local storage, catching, in memory)
+                localStorage.removeItem('access-token');
+            }
             console.log('current user', currentUser);
             setLoading(false);
         });
