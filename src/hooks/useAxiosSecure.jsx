aa772ac7b84c70b5dc5f9f29d6c7a8373793ffe8
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-28 21:21:57 +0600

    navigate to login page unauthorized access

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,14 +1,19 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create ({
     baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () =>{
+    const navigate = useNavigate();
+    const {logout} = useAuth();
+
     // request interceptor to add authorization header for every secure call to teh api
     axiosSecure.interceptors.request.use(function(config){
         const token = localStorage.getItem('access-token');
-        console.log('request stopped by interceptors', token);
+        // console.log('request stopped by interceptors', token);
         config.headers.authorization = `Bearer ${token}`
         return config;
     }, function (error) {
@@ -22,6 +27,10 @@ const useAxiosSecure = () =>{
     }, (error) => {
         const status = error.response.status;
         console.log('status error in the interceptor', status);
+        // for 401 or 403 logout the user and move the user to the login
+        if(status === 401 || status === 403){
+            navigate('/login')
+        }
         return Promise.reject(error);
     })
     return axiosSecure;
