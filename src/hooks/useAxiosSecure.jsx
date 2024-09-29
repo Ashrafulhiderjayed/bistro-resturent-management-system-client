ffe729371dc19c1659fe26b73910425776f83c4a
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-29 16:47:34 +0600

    hook for admin

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,16 +2,16 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
-const axiosSecure = axios.create ({
+const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
-const useAxiosSecure = () =>{
+const useAxiosSecure = () => {
     const navigate = useNavigate();
-    const {logout} = useAuth();
+    const { logOut } = useAuth();
 
     // request interceptor to add authorization header for every secure call to teh api
-    axiosSecure.interceptors.request.use(function(config){
+    axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token');
         // console.log('request stopped by interceptors', token);
         config.headers.authorization = `Bearer ${token}`
@@ -19,18 +19,18 @@ const useAxiosSecure = () =>{
     }, function (error) {
         // Do something with request error
         return Promise.reject(error);
-      });
+    });
 
     // response interceptor to handle unauthorized access and refresh tokens
-    axiosSecure.interceptors.response.use(function(response){
+    axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, async(error) => {
+    }, async (error) => {
         const status = error.response.status;
-        console.log('status error in the interceptor', status);
-        // for 401 or 403 logout the user and move the user to the login page
-        if(status === 401 || status === 403){
-            await logout();
-            navigate('/login')
+        // console.log('status error in the interceptor', status);
+        // for 401 or 403 logout the user and move the user to the login
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
         }
         return Promise.reject(error);
     })
