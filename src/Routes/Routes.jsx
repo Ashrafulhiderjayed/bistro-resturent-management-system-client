e1ca2965f0ca3e0fbf64260939ba5a2319e8ecc6
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-04 16:38:45 +0600

    protect ManageItems into AdminRoute

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import Cart from "../Pages/Dashboard/Cart/Cart";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
+import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 
 
   export const router = createBrowserRouter([
@@ -61,6 +62,10 @@ import AdminRoute from "./AdminRoute";
         {
           path: "addItems",
           element: <AdminRoute><AddItems /></AdminRoute>,
+        },
+        {
+          path: "manageItems",
+          element: <AdminRoute><ManageItems /></AdminRoute>,
         },
         {
           path: "users",
