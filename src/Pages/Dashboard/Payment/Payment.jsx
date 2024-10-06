29981cf229efac70180ac2babe2965ece4fb95bf
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-06 17:25:11 +0600

    stripe installed and component created for  payment

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
+        </div>
+    );
+};
+
+export default Payment;
