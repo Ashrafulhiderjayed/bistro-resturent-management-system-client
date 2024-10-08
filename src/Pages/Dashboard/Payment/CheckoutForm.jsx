55bcd6e321b3fe96e4bd23289bb5b5b18ae446e0
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-08 14:23:21 +0600

    checkout form with error message

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,24 +1,64 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import React from 'react';
 
 const CheckoutForm = () => {
-    const handleSubmit = async(event) => {
+    const [error, setError] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+    
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        // Your form submission logic goes here
-        // You can use the `fetch()` API to send the form data to your server
-        // const formData = new FormData(event.target);
-        // const response = await fetch('/submit-order', {
-        //     method: 'POST',
-        //     body: formData
-        // });
-        // const data = await response.json();
-        // console.log(data);
-        // Clear the form inputs
-        // event.target.reset();
-        // Redirect to a success page or display a success message
+
+        if(!stripe || !elements){
+            console.error('stripe or elements not initialized');
+            return;
+        }
+
+        const card = elements.getElement(CardElement)
+
+        if(card == null){
+            console.error('No card element found');
+            return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if(error) {
+            console.error('Error creating payment method', error);
+            setError(error.message);
+            return;
+        }
+
+        else{
+            console.log('Payment method', paymentMethod);
+            setError('');
+        }
     }
     return (
-        <form onSubmit={}>
-            
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className='btn btn-sm btn-primary my-4' type="submit" disabled={!stripe}>
+                Pay
+            </button>
+            <p className='text-red-500'>{error}</p>
         </form>
     );
 };
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
             <div>
-                <Elements stripe={stripePromise}></Elements>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
             </div>
         </div>
     );
