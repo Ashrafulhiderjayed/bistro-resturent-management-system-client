4b8755ef0b2259b3a7abfca36466dd62cf8237b6
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-10-10 12:33:55 +0600

    custom shape BarChart implemented

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -3,12 +3,16 @@ import useAuth from '../../../hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FaBook, FaDollarSign, FaUsers } from 'react-icons/fa';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const AdminHome = () => {
-    const {user } = useAuth();
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const {data: stats={}}  = useQuery({
+    const { data: stats = {} } = useQuery({
         queryKey: ['admin-stats'],
         queryFn: async () => {
             const res = await axiosSecure.get('admin-stats');
@@ -17,7 +21,21 @@ const AdminHome = () => {
         // refetchInterval: 60000,  // Update every minute
     });
 
-    const {data: chartData} = useQuery({
+    //custom shape for the bar chart
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
+    const { data: chartData = [] } = useQuery({
         queryKey: ['order-stats'],
         queryFn: async () => {
             const res = await axiosSecure.get('/order-stats');
@@ -76,6 +94,32 @@ const AdminHome = () => {
                     <div className="stat-desc">↘︎ 90 (14%)</div>
                 </div>
 
+            </div>
+            <div className="flex">
+                {/* Bar chart */}
+                <div className='w-1/2'>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                <div className='w-1/2'></div>
             </div>
         </div>
     );
